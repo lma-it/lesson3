06bd34516046372585e136c30ecdbea1badc8650
Михаил Лубенцов <wispergently061@gmail.com>
2024-02-12 16:19:17 +0500

    Create and define the function

---
+++ Task1_Functions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+double result = CalculateFormula(6, 10, 3, 3);
+printResult(result);
+
+
+double CalculateFormula(double a, double b, double c, double d){
+
+    return (a * b) / (c + d);
+}
+
+void printResult(double result){
+
+    System.Console.WriteLine(result);
+
+}
